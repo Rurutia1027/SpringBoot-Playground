f40f737a9661a327fa594bd7a97e4bfcd734462b
Rurutia1027 <rurutia1027@gmail.com>
2024-12-26 22:54:04 +0800

    update notes for React, React Components, React Components, React Components Type, React Component render  and lifecycle

--- springboot-react-fullstack/src/frontend/src/App.js
+++ springboot-react-fullstack/src/frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
+import {getAllStudents} from "./client";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // here we create an initial and empty  state
+    const [students, setStudents] = useState([]);
+    const fetchStudents = () => {
+        getAllStudents()
+            .then(response => response.json())
+            .then(data => setStudents(data));
+    };
+
+    useEffect(() => {
+        console.log("component is mounted ");
+        fetchStudents();
+    }, []);
+
+
+  return <p>{students.length}</p>;
 }
 
 export default App;
