eebb29ff9ad5da0b33c04bb775bb6cf9c1bd7edd
Rurutia1027 <rurutia1027@gmail.com>
2024-12-30 14:28:39 +0800

    update service and repo for db layer

--- springboot-react-fullstack/src/main/java/com/cloud/fullstack/student/Student.java
+++ springboot-react-fullstack/src/main/java/com/cloud/fullstack/student/Student.java
@@ -8,16 +8,40 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 @ToString
 @Getter
 @Setter
-@NoArgsConstructor
 @EqualsAndHashCode
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1)
+    @GeneratedValue(
+            generator = "student_sequence", strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String email;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
+
+    public Student(String name, String email, Gender gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
 }
---
+++ springboot-react-fullstack/src/main/java/com/cloud/fullstack/student/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.cloud.fullstack.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
