604b0d9f223d8796648257ae660f5d3965fd81a3
Rurutia1027 <rurutia1027@gmail.com>
2025-02-18 22:54:07 +0800

    finish demo codes for springboot integrate with graphql and its simple unit test cases

--- spring-boot-graphql/src/main/java/com/cloud/graphqlspring/Book.java
+++ spring-boot-graphql/src/main/java/com/cloud/graphqlspring/Book.java
@@ -2,12 +2,17 @@ package com.cloud.graphqlspring;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-public record Book(Integer id, String name, Integer pageCount) {
+public record Book(Integer id, String name, Integer pageCount, Integer authorId) {
     public static List<Book> books = Arrays.asList(
-            new Book(1, "Quran", 604),
-            new Book(2, "Harry Potter", 700),
-            new Book(3, "Foobar", 2342),
-            new Book(1, "Spring Boot", 234)
+            new Book(1, "Quran", 604, 1),
+            new Book(2, "Harry Potter", 700, 2),
+            new Book(3, "Foobar", 2342, 3),
+            new Book(1, "Spring Boot", 234, 2)
     );
+
+    public static Optional<Book> getBookById(Integer id) {
+        return books.stream().filter(item -> item.id.equals(id)).findFirst();
+    }
 }
