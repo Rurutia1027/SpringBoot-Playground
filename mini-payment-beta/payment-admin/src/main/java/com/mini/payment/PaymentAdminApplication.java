172c0cbce5cd8f06c2a59cf2f9b3f531cb3b4c31
Rurutia1027 <rurutia1027@gmail.com>
2025-04-08 12:32:19 +0800

    add admin web implementation

---
+++ mini-payment-beta/payment-admin/src/main/java/com/mini/payment/PaymentAdminApplication.java
@@ -0,0 +1,11 @@
+package com.mini.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentAdminApplication.class, args);
+    }
+}
---
+++ mini-payment-beta/payment-admin/src/main/java/com/mini/payment/account/AccountController.java
@@ -0,0 +1,12 @@
+package com.mini.payment.account;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello";
+    }
+}
