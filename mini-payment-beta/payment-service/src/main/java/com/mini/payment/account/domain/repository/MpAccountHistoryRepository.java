1bdad13a5d000af40d57f592191cf3173be9b898
Rurutia1027 <rurutia1027@gmail.com>
2025-04-08 15:03:14 +0800

    update codes

--- mini-payment-beta/payment-service/src/main/java/com/mini/payment/account/domain/repository/MpAccountHistoryRepository.java
+++ mini-payment-beta/payment-service/src/main/java/com/mini/payment/account/domain/repository/MpAccountHistoryRepository.java
@@ -1,19 +1,14 @@
 package com.mini.payment.account.domain.repository;
 
 import com.mini.payment.account.domain.entity.MpAccountHistory;
-import com.mini.payment.account.domain.repository.custom.MpAccountHistoryRepositoryCustom;
 import com.mini.payment.repository.BaseRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
-
 @Repository
-public interface MpAccountHistoryRepository extends  BaseRepository<MpAccountHistory>, MpAccountHistoryRepositoryCustom {
+public interface MpAccountHistoryRepository extends BaseRepository<MpAccountHistory> {
     @Modifying
     @Query("UPDATE MpAccountHistory a SET a.amount = :#{#e.amount}, a.balance = :#{#e.balance} WHERE a.id = :#{#e.id}")
     void updateAmountAndBalance(@Param("e") MpAccountHistory e);
--- mini-payment-beta/payment-service/src/main/java/com/mini/payment/account/domain/service/MpAccountHistoryService.java
+++ mini-payment-beta/payment-service/src/main/java/com/mini/payment/account/domain/service/MpAccountHistoryService.java
@@ -1,6 +1,5 @@
 package com.mini.payment.account.domain.service;
 
-import com.mini.payment.account.domain.dto.MpAccountHistoryCondition;
 import com.mini.payment.account.domain.entity.MpAccountHistory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
